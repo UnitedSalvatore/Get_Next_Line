02f9feaa758225410b07ee07ba593e077013612d
Yaroslav PIKUL <ypikul@e2r6p5.unit.ua>
2017-11-29 22:23:05 +0200

    Add libft

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,37 @@
 /*   By: ypikul <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 04:32:10 by ypikul            #+#    #+#             */
-/*   Updated: 2017/11/26 04:32:13 by ypikul           ###   ########.fr       */
+/*   Updated: 2017/11/29 22:20:42 by ypikul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
-int get_next_line(const int fd, char **line)
+t_list	*get_elem(const int fd, t_list **list)
+{
+	t_list			*current;
+	char			*buff[BUFF_SIZE + 1];
+
+	current = *list;
+	while (current)
+	{
+		if (current->content_size == fd)
+			return (current);
+		current = current->next;
+	}
+
+	if ((current = ft_lstnew()))
+}
+
+int		get_next_line(const int fd, char **line)
 {
 	static t_list	*list;
 	t_list			*current;
+
 	if (fd < 0 || line == NULL || BUFF_SIZE < 1)
 		return (-1);
+
 
 	return (0);
 }

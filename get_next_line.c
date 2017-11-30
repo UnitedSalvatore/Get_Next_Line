c018143e22a7377ea1c142882e9f1dd597259c46
Yaroslav PIKUL <ypikul@e2r6p5.unit.ua>
2017-11-30 20:29:40 +0200

    Update GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,38 @@
 /*   By: ypikul <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/26 04:32:10 by ypikul            #+#    #+#             */
-/*   Updated: 2017/11/29 22:20:42 by ypikul           ###   ########.fr       */
+/*   Updated: 2017/11/30 16:48:46 by ypikul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
-static t_list_fd	*get_current_fd(const int fd, t_list **list)
+static *t_list_fd	*read_file(const int fd, t_list *current)
+{
+	char	*buf[BUFF_SIZE + 1];
+	char	*str;
+	int		ret;
+
+	while (!(ft_strchr(current->content, '\n')) || \
+			(ret = read(fd, buf, BUFFSIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		if (current->content)
+		{
+			str = current->content;
+			current->content = ft_strjoin(current->content, buf);
+			ft_strdel(&str);
+			if (current->content == NULL)
+				return (NULL);
+		}
+		else
+			current->content = ft_strdup(buf);
+	}
+	return (current);
+}
+
+static *t_list_fd	*get_current_fd(const int fd, t_list **list)
 {
 	t_list_fd	*current;
 
@@ -37,7 +61,6 @@ int					get_next_line(const int fd, char **line)
 {
 	static t_list_fd	*list;
 	t_list_fd			*current;
-	char				*buf[BUFF_SIZE + 1];
 
 	if (fd < 0 || line == NULL || BUFF_SIZE < 1 || read(fd, buf, 0) < 0)
 		return (-1);

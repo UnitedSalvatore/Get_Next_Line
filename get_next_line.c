2f48cbe9d9180f8d62bdc0ece7df4574f64e5030
Yaroslav <salvaincorp@gmail.com>
2017-12-01 03:42:23 +0200

    Update GNL

--- get_next_line.c
+++ get_next_line.c
@@ -13,26 +13,39 @@
 #include "get_next_line.h"
 #include "libft.h"
 
+static void			get_line(t_list_fd *curr, char **line)
+{
+	char	*ptr;
+
+	if (curr->content && *(curr->content) != '\0')
+	{
+		if (!(ptr = ft_strchr(curr->content, '\n')) && \
+				!(*line = ft_strdup(curr->content)))
+			return ;
+		else
+			if (!(*line = ft_strsub(curr->content, 0, ptr - curr->content)))
+				return ;
+		if
+	}
+	else
+		*line = NULL;
+}
+
 static *t_list_fd	*read_file(const int fd, t_list *current)
 {
 	char	*buf[BUFF_SIZE + 1];
 	char	*str;
 	int		ret;
 
-	while (!(ft_strchr(current->content, '\n')) || \
-			(ret = read(fd, buf, BUFFSIZE)) > 0)
+	while (!(ft_strchr(current->content, '\n')) \
+			&& (ret = read(fd, buf, BUFFSIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		if (current->content)
-		{
-			str = current->content;
-			current->content = ft_strjoin(current->content, buf);
-			ft_strdel(&str);
-			if (current->content == NULL)
-				return (NULL);
-		}
-		else
-			current->content = ft_strdup(buf);
+		str = current->content;
+		current->content = ft_strjoin(current->content, buf);
+		ft_strdel(&str);
+		if (!(current->content))
+			return (NULL);
 	}
 	return (current);
 }
@@ -48,10 +61,14 @@ static *t_list_fd	*get_current_fd(const int fd, t_list **list)
 			return (current);
 		current = current->next;
 	}
-	if ((current = malloc(sizeof(*current))) == NULL)
+	if (!(current = malloc(sizeof(*current))))
 		return (NULL);
 	current->fd = fd;
-	current->content = NULL;
+	if (!(current->content = ft_strdup('\0')))
+	{
+		free(current);
+		return (NULL);
+	}
 	current->next = *list;
 	*list = current;
 	return (current);
@@ -62,10 +79,11 @@ int					get_next_line(const int fd, char **line)
 	static t_list_fd	*list;
 	t_list_fd			*current;
 
-	if (fd < 0 || line == NULL || BUFF_SIZE < 1 || read(fd, buf, 0) < 0)
+	if (fd < 0 || !line || BUFF_SIZE < 1 || read(fd, buf, 0) < 0)
 		return (-1);
-	if ((current = get_current_fd(fd, &list)) == NULL)
+	if (!(current = get_current_fd(fd, &list)) || \
+			!read_file(fd, current))
 		return (-1);
-
-	return (0);
+	get_line(current, line);
+	return ();
 }
